b98945b0249b47941576858d7133fcc8f571053c
Marcelo <marcelo@hibejix.local>
2018-07-28 01:27:57 -0300

    Metodos para verificar se a noticia foi revisada pelos revisores

--- src/main/java/br/com/hibejix/cms/domains/models/News.java
+++ src/main/java/br/com/hibejix/cms/domains/models/News.java
@@ -22,4 +22,25 @@ public class News {
     private Set<Category> categories;
 
     private Set<Tag> tags;
+
+
+    /*
+     * Check if the news was reviewed by all mandatory reviewers
+     */
+    public Review review(final String userId, final String status) {
+        final Review review = new Review(userId, status);
+        this.reviewrs.add(review);
+        return review;
+    }
+
+    /*
+     * Check if the news was fully revised by all mandatory reviewers
+     */
+    public Boolean revised() {
+        return this.mandatoryReviwers.stream().allMatch(
+                reviewr -> this.reviewrs.stream().anyMatch(
+                        review -> reviewr.getId().equals(review.getUserId()) && "approved".equals(review.getStatus())
+                )
+        );
+    }
 }
